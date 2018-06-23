aac26ea588f0af186964b094fd650bd7afc375c6
yufw <yynyygy@gmail.com>
2018-06-23 21:06:48 +0800

    rewrite

--- snake.py
+++ snake.py
@@ -1,78 +1,114 @@
-#!/usr/bin/env python3.3
-
-import curses
 import random
+import select
+import sys
+import termios
 import time
-import signal
+import tty
 
+class Snake:
+    def __init__(self):
+        self.body = [(5, 5), (6, 5), (7, 5)] # tail to head
+        self.direction = 'right'
 
-def redraw(window, snake):
-    window.erase()
-    for i in range(curses.LINES):
-        for j in range(curses.COLS):
-            if snake[i][j] != 0:
-                window.addch(i, j, '*')
-    window.refresh()
+    def setdir(self, direction): # cannot turn back
+        dirs = ['left', 'up', 'down', 'right']
+        if (dirs.index(self.direction) + dirs.index(direction)) != 3:
+            self.direction = direction
 
-def food(snake):
-    y = random.randint(0, curses.LINES)
-    x = random.randint(0, curses.COLS)
-    while snake[y][x] != 0:
-        y = random.randint(0, curses.LINES)
-        x = random.randint(0, curses.COLS)
-    snake[y][x] = -1
+    def move(self, food):
+        direction = self.direction
+        x, y = self.body[-1]
+        if direction == 'right':
+            x += 1
+        elif direction == 'left':
+            x -= 1
+        elif direction == 'up':
+            y -= 1
+        elif direction == 'down':
+            y += 1
+        newhead = (x, y)
+        self.body.append(newhead)
+        if newhead != (food.x, food.y): # not eating
+            self.body.pop(0)
+        return newhead
 
-def run():
-    stdscr = curses.initscr()
-    stdscr.keypad(True)
-    curses.curs_set(False)
-    stdscr.border(0)
+class Food:
+    def __init__(self, xmax, ymax):
+        self.xmax = xmax
+        self.ymax = ymax
 
-    snake = [[0 for i in range(curses.COLS)] for j in range(curses.LINES)]
-    snake[curses.LINES // 2][curses.COLS // 2] = 1
-    data = {'drow': 0, 'dcol': 1, 'total_nodes': 1,
-            'head_y': curses.LINES // 2, 'head_x': curses.COLS // 2}
+    def feed(self):
+        self.x = random.randint(0, self.xmax)
+        self.y = random.randint(0, self.ymax)
 
-    food(snake)
-    redraw(stdscr, snake)
-    # move(stdscr, snake, data)
+class Game:
+    def __init__(self, height, width):
+        self.board = (height, width) # height and width
+        self.snake = Snake()
+        self.food = Food(width-1, height-1)
+        self.food.feed()
+        self.paused = False
 
-    def move(signum, frame):
-        data['head_y'] = data['head_y'] + data['drow']
-        data['head_x'] = data['head_x'] + data['dcol']
-        if snake[data['head_y']][data['head_x']] == -1:
-            snake[data['head_y']][data['head_x']] = data['total_nodes'] + 1
-            data['total_nodes'] = data['total_nodes'] + 1
-            food(snake)
-        elif snake[data['head_y']][data['head_x']] == 0:
-            snake[data['head_y']][data['head_x']] = data['total_nodes'] + 1
-            for i in range(curses.LINES):
-                for j in range(curses.COLS):
-                    if snake[i][j] > 0:
-                        snake[i][j] = snake[i][j] - 1
-        redraw(stdscr, snake)
+    def render(self):
+        sys.stdout.write("\x1b[2J\x1b[H")
+        board = [[' ' for x in range(self.board[1])] for y in range(self.board[0])]
+        for c in self.snake.body:
+            board[c[1]][c[0]] = 'x'
+        board[self.food.y][self.food.x] = '*'
+        for line in board:
+            sys.stdout.write(''.join(line) + '<' + '\r\n')
+        sys.stdout.write('^' * (self.board[1] + 1) + '\r\n')
+        sys.stdout.flush()
+            
+    def loop(self, old):
+        while True:
+            self.render()
+            ch = None
+            rl, _, _ = select.select([sys.stdin], [], [], 0.17)
+            if rl:
+                ch = sys.stdin.read(1)
+                if ch == 'q':
+                    self.quit(old)
+                    return
+                elif ch == 'p':
+                    self.paused = not self.paused
+            if self.paused:
+                continue
+            elif ch == 'l':
+                self.snake.setdir('right')
+            elif ch == 'j':
+                self.snake.setdir('left')
+            elif ch == 'k':
+                self.snake.setdir('down')
+            elif ch == 'i':
+                self.snake.setdir('up')
+            else:
+                self.snake.setdir(self.snake.direction)
+            newhead = self.snake.move(self.food)
+            if not self.check(newhead):
+                self.quit(old)
+                return
+            if newhead == (self.food.x, self.food.y): # it is eating
+                self.food.feed()
 
-    def none_handler():
-        pass
+    def quit(self, oldsetting):
+        sys.stdout.write('life is too long' + '\r\n')
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, oldsetting)
+        sys.stdout.write("\033[?25h")
 
-    signal.signal(signal.SIGALRM, move)
-    while True:
-        signal.alarm(1)
-        c = stdscr.getch()
-        if c == ord('q'):
-            stdscr.keypad(False)
-            curses.endwin()
-            signal.signal(signal.SIGTERM, none_handler)
-        elif c == curses.KEY_LEFT:
-            data['drow'] = 0
-            data['dcol'] = -1
-        elif c == curses.KEY_RIGHT:
-            data['drow'] = 0
-            data['dcol'] = 1
-        elif c == curses.KEY_UP:
-            data['drow'] = -1
-            data['dcol'] = 0
-        elif c == curses.KEY_DOWN:
-            data['drow'] = 1
-            data['dcol'] = 0
-run()
+    def check(self, newhead):
+        x, y = newhead
+        if 0 <= x < self.board[1] and 0 <= y < self.board[0]:
+            return True
+        else:
+            return False
+
+    def main(self):
+        old = termios.tcgetattr(sys.stdin)
+        tty.setraw(sys.stdin)
+        sys.stdout.write("\033[?25l")
+        self.loop(old)
+
+if __name__ == '__main__':
+    g = Game(20, 40)
+    g.main()
